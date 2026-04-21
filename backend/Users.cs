f747f35494bad3b3745280e3444b9bf534a3e258
GavinBlesh <bleshgavin@gmail.com>
2026-04-21 16:47:14 -0500

    Implemented password hashing in the backend

--- backend/Users.cs
+++ backend/Users.cs
@@ -14,23 +14,31 @@ public class Users {
     /*
     * Information to create this was sourced from:
     * https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlcommand.executescalar?view=netframework-4.8.1
+    *
+    * Information to create and store the hashed password was sourced from:
+    * https://claudiobernasconi.ch/blog/how-to-hash-passwords-with-bcrypt-in-csharp/
     */
     public bool login(String email, String password){
-        string query = "SELECT userID from Users where email = @Email AND password = @Password";
+        string query = "SELECT userID, password from Users where email = @Email";
 
         try {
             using (DatabaseConnection database = new DatabaseConnection()){
                 using (SqlCommand command = new SqlCommand(query, database.OpenConnection())){
                     command.Parameters.Add("@Email", System.Data.SqlDbType.VarChar, 255).Value = email;
-                    command.Parameters.Add("@Password", System.Data.SqlDbType.VarChar, 255).Value = password;
                     
-                    object result = command.ExecuteScalar();
+                    using (SqlDataReader reader = command.ExecuteReader()) {
+                        if (reader.Read()) {
+                            string storedHash = reader["password"].ToString();
+                            bool isPasswordCorrect = false;
+                            isPasswordCorrect = BCrypt.Net.BCrypt.Verify(password, storedHash);
 
-                    if (result != null){
-                        this.userID = Convert.ToInt32(result);
-                        this.email = email;
-                        this.password = password;
-                        return true;
+                            if (isPasswordCorrect) {
+                                this.userID = Convert.ToInt32(reader["userID"]);
+                                this.email = email;
+                                this.password = storedHash;
+                                return true;
+                            }
+                        }
                     }
                 }
             }
@@ -43,11 +51,12 @@ public class Users {
 
 
     public void createAccount(String email, String password){
+        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
         string query = "INSERT INTO users (email, password) VALUES (@Email, @Password)";
         using (DatabaseConnection db = new DatabaseConnection()){
             using (SqlCommand command = new SqlCommand(query, db.OpenConnection())){
                 command.Parameters.Add("@Email", System.Data.SqlDbType.VarChar, 255).Value = email;
-                command.Parameters.Add("@Password", System.Data.SqlDbType.VarChar, 255).Value = password;
+                command.Parameters.Add("@Password", System.Data.SqlDbType.VarChar, 255).Value = hashedPassword;
                 command.ExecuteNonQuery();
             }
         }
