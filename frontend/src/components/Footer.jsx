4605e8059a4aa66e0da530c212aa77ec5dd52e7c
GavinBlesh <bleshgavin@gmail.com>
2026-04-27 16:40:51 -0500

    removed the unneeded footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,31 +5,8 @@ export default function Footer() {
         <footer className="footer">
             <div className="footer__grid">
                 <div className="footer__col">
-                    <h4>STORE</h4>
-                    <ul>
-                        <li>About Us</li>
-                        <li>Careers</li>
-                        <li>Press</li>
-                    </ul>
+                    <h4>Online Store</h4>
                 </div>
-                <div className="footer__col">
-                    <h4>HELP</h4>
-                    <ul>
-                        <li>FAQ</li>
-                        <li>Shipping Info</li>
-                        <li>Returns</li>
-                    </ul>
-                </div>
-                <div className="footer__col">
-                    <h4>CONTACT</h4>
-                    <ul>
-                        <li>support@store.com</li>
-                        <li>1-800-STORE</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="footer__bottom">
-                <p>© 2024 STORE. All rights reserved.</p>
             </div>
         </footer>
     );
