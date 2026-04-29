e4769e76b1ee16711e01d906060910decea2ed5d
GavinBlesh <bleshgavin@gmail.com>
2026-04-29 13:26:46 -0500

    Started adding comments and cleaning up code

--- backend/AuthRequest.cs
+++ backend/AuthRequest.cs
@@ -1,3 +1,7 @@
+/*
+* Contains the user's login credentials
+* and carries it over to the login/registration backend controllers
+*/
 public class AuthRequest {
     public string Email { get; set; }
     public string Password { get; set; }
